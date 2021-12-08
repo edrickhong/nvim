f1f323f553d99287544a5b2f336937b192dbc261
ehzx <edrickhong@gmail.com>
2021-12-07 21:51:00 -0500

    reindent code

--- init.lua
+++ init.lua
@@ -4,36 +4,37 @@ vim.opt.listchars:append("eol:↴")
 require('plugins')
 
 
+--TODO move this to plugins
 require('marks').setup {
-  -- whether to map keybinds or not. default true
-  default_mappings = true,
-  -- which builtin marks to show. default {}
-  builtin_marks = { ".", "<", ">", "^" },
-  -- whether movements cycle back to the beginning/end of buffer. default true
-  cyclic = true,
-  -- whether the shada file is updated after modifying uppercase marks. default false
-  force_write_shada = false,
-  -- how often (in ms) to redraw signs/recompute mark positions. 
-  -- higher values will have better performance but may cause visual lag, 
-  -- while lower values may cause performance penalties. default 150.
-  refresh_interval = 250,
-  -- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
-  -- marks, and bookmarks.
-  -- can be either a table with all/none of the keys, or a single number, in which case
-  -- the priority applies to all marks.
-  -- default 10.
-  sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
-  -- disables mark tracking for specific filetypes. default {}
-  excluded_filetypes = {},
-  -- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
-  -- sign/virttext. Bookmarks can be used to group together positions and quickly move
-  -- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
-  -- default virt_text is "".
-  bookmark_0 = {
-    sign = "⚑",
-    virt_text = "hello world"
-  },
-  mappings = {}
+	-- whether to map keybinds or not. default true
+	default_mappings = true,
+	-- which builtin marks to show. default {}
+	builtin_marks = { ".", "<", ">", "^" },
+	-- whether movements cycle back to the beginning/end of buffer. default true
+	cyclic = true,
+	-- whether the shada file is updated after modifying uppercase marks. default false
+	force_write_shada = false,
+	-- how often (in ms) to redraw signs/recompute mark positions. 
+	-- higher values will have better performance but may cause visual lag, 
+	-- while lower values may cause performance penalties. default 150.
+	refresh_interval = 250,
+	-- sign priorities for each type of mark - builtin marks, uppercase marks, lowercase
+	-- marks, and bookmarks.
+	-- can be either a table with all/none of the keys, or a single number, in which case
+	-- the priority applies to all marks.
+	-- default 10.
+	sign_priority = { lower=10, upper=15, builtin=8, bookmark=20 },
+	-- disables mark tracking for specific filetypes. default {}
+	excluded_filetypes = {},
+	-- marks.nvim allows you to configure up to 10 bookmark groups, each with its own
+	-- sign/virttext. Bookmarks can be used to group together positions and quickly move
+	-- across multiple buffers. default sign is '!@#$%^&*()' (from 0 to 9), and
+	-- default virt_text is "".
+	bookmark_0 = {
+		sign = "⚑",
+		virt_text = "hello world"
+	},
+	mappings = {}
 }
 
 -- color scheme
@@ -135,16 +136,16 @@ map('n','<f1>',':wa | lua LinuxBuild()<CR>')
 
 
 function dump(o)
-   if type(o) == 'table' then
-      local s = '{ '
-      for k,v in pairs(o) do
-         if type(k) ~= 'number' then k = '"'..k..'"' end
-         s = s .. '['..k..'] = ' .. dump(v) .. ','
-      end
-      return s .. '} '
-   else
-      return tostring(o)
-   end
+	if type(o) == 'table' then
+		local s = '{ '
+		for k,v in pairs(o) do
+			if type(k) ~= 'number' then k = '"'..k..'"' end
+			s = s .. '['..k..'] = ' .. dump(v) .. ','
+		end
+		return s .. '} '
+	else
+		return tostring(o)
+	end
 end
 
 
@@ -154,16 +155,16 @@ function OpenQuickFixList()
 end
 
 function ReloadConfig()
-    local hls_status = vim.v.hlsearch
-    for name,_ in pairs(package.loaded) do
-        if name:match('^cnull') then
-            package.loaded[name] = nil
-        end
-    end
-    dofile(vim.env.MYVIMRC)
-    if hls_status == 0 then
-        vim.opt.hlsearch = false
-    end
+	local hls_status = vim.v.hlsearch
+	for name,_ in pairs(package.loaded) do
+		if name:match('^cnull') then
+			package.loaded[name] = nil
+		end
+	end
+	dofile(vim.env.MYVIMRC)
+	if hls_status == 0 then
+		vim.opt.hlsearch = false
+	end
 end
 
 
@@ -198,7 +199,7 @@ nvim_create_augroups({
 	},
 
 	--quickfix = {
-		--{'QuickFixCmdPost','[^l]*','lua OpenQuickFixList'},
+	--{'QuickFixCmdPost','[^l]*','lua OpenQuickFixList'},
 	--},
 })
 
